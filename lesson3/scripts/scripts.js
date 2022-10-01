4ef1b4974aaecae328b208eb7599e06b5c88b381
Pangloss1107 <pablonsantos1107@gmail.com>
2022-10-01 18:36:31 -0300

    lesson3

---
+++ lesson3/scripts/scripts.js
@@ -0,0 +1,44 @@
+let monthnames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+
+];
+
+let daynames = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday"
+]
+
+//To find and print the current Year
+const options = {weekday:"long", day:"numeric", month: "long", year: "numeric"};
+const d = new Date();
+const year = d.getFullYear();
+document.getElementById("year3").textContent = year;
+
+
+   
+//To show the las updated 
+let oLastModif = new Date(document.lastModified);
+let dayModif = oLastModif.getDate() 
+let monthModif = oLastModif.getMonth()
+let yearModif = oLastModif.getFullYear() 
+let hourModif = oLastModif.getHours()
+let minuteModif = oLastModif.getMinutes()
+let secondsModif = oLastModif.getSeconds()
+let completeMod = `Last Updated: ${dayModif}/${monthModif + 1}/${yearModif} ${hourModif}:${minuteModif}:${secondsModif}`
+document.getElementById("modified3").textContent = completeMod;
