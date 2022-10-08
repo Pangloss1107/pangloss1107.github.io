c43c84e852e64bd7ef6ce9351efb4d0766695177
Pangloss1107 <pablonsantos1107@gmail.com>
2022-10-08 01:01:43 -0300

    update

---
+++ chamber/scripts/scripts.js
@@ -0,0 +1,74 @@
+//This is for Hamburguer Menu 
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("open")
+    document.getElementById("hamburgerBtn").classList.toggle("open")
+ }
+ 
+ const x = document.getElementById("hamburgerBtn")
+ x.onclick = toggleMenu;
+
+ //Footer Info 
+
+ let monthnames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+
+];
+
+let daynames = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday"
+]
+
+//To find and print the current Year
+const options = {weekday:"long", day:"numeric", month: "long", year: "numeric"};
+const d = new Date();
+const year = d.getFullYear();
+document.getElementById("year").textContent = year;
+
+
+   
+//To show the las updated 
+let oLastModif = new Date(document.lastModified);
+let dayModif = oLastModif.getDate() 
+let monthModif = oLastModif.getMonth()
+let yearModif = oLastModif.getFullYear() 
+let hourModif = oLastModif.getHours()
+let minuteModif = oLastModif.getMinutes()
+let secondsModif = oLastModif.getSeconds()
+let completeMod = `Last Updated: ${dayModif}/${monthModif + 1}/${yearModif} ${hourModif}:${minuteModif}:${secondsModif}`
+document.getElementById("modified").textContent = completeMod;
+
+
+
+// select the elements to manipulate (output to)
+const datefield = document.querySelector(".date");
+const datefieldUK = document.querySelector("aside"); // for european/family history format with day first.
+
+// derive the current date using a date object
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+	now
+);
+const fulldateUK = new Intl.DateTimeFormat("en-UK", {
+	dateStyle: "full"
+}).format(now);
+// long, medium, short options ... try them
+
+datefield.innerHTML = `<em>${fulldate}</em>`;
+datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
