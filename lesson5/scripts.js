b4d38f5a4591b1f165e8143d068d458901a24b4a
Pangloss1107 <pablonsantos1107@gmail.com>
2022-10-14 02:29:45 -0300

    update

---
+++ lesson5/scripts.js
@@ -0,0 +1,31 @@
+const input = document.querySelector('input')
+
+const button = document.querySelector('button')
+
+const list = document.querySelector('ul')
+
+button.addEventListener('click', function() {
+    if (input.value.length === 0){
+        console.log("Please add a chapter of the Book of Mormon")
+    } else {
+        const items = input.value;
+    
+
+    const listofItems = document.createElement("li");
+    const listofText = document.createElement("span")
+    const listofButtons = document.createElement("button")
+
+
+    listofItems.appendChild(listofText);
+    listofText.textContent = items;
+    listofItems.appendChild(listofButtons);
+    listofButtons.textContent = "❌"; 
+
+    list.appendChild(listofItems)
+
+    listofButtons.addEventListener("click", function () {
+        list.removeChild(listofItems)
+    });
+    input.focus();
+}
+})
