ca40d80910139891637b4456b7b4e01fc180a424
Pangloss1107 <pablonsantos1107@gmail.com>
2022-10-22 18:22:21 -0300

    update

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,16 +1,18 @@
-let windchill = document.querySelector(".windchill");
-let temperature = document.querySelector(".tempfaren");
-let windspeed = document.querySelector(".windspeed");
 
-if (temperature <= 50 && windspeed > 3) {
+let temperature = parseFloat(document.querySelector('.tempfaren').textContent); 
+let windspeed =   parseFloat(document.querySelector('.windspeed').textContent);
+document.querySelector('.windchill').textContent = windChill(temperature, windspeed);;
+
+function windChill(temperature, windspeed) {
+    if (temperature <=50 && windspeed >= 3){
+
+        let windchill = 35.74 + 0.6215 * temperature - 35.75 * (Math.pow(windspeed, 0.16)) + 0.4275 * temperature * (Math.pow(windspeed, 0.16));
+        return windchill.toFixed(1) + "°" + "F";
+    } else{
+        return windchill = "N/A"
+    }
     
-    let chill = 35.74 + 0.6215(temperature) - 35.75(windspeed **0.16) + 
-    0.4275(temperature)(windspeed ** 0.16);
-
-    windchill.innerHTML = `${chill}`; 
-} else{
-    chill = "N/A";
-
-    windchill.innerHTML = `${chill}`; 
 }
+
+
 
