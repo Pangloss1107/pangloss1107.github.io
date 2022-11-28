c4a7a185acb7add5637c31c86dc251b8c8041baa
Pangloss1107 <pablonsantos1107@gmail.com>
2022-11-28 14:29:54 -0300

    updates

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -100,42 +100,47 @@ if (actualDay === 1 || actualDay === 2) {
   
 }
 
+// Spotlights
+/*const requestURL = "https://pangloss1107.github.io/chamber/directory/scripts/data.json";
+const cards = document.querySelector('spotlights');
 
-const requestURL =
-'https://pangloss1107.github.io/chamber/directory/scripts/data.json'
-const cards = document.querySelector('.spotlights');
-cards.innerHTML = '';
 
 async function getData() {
   const request = new Request(requestURL);
   const response = await fetch(request);
   const jsonObject = await response.json();
-  const allClients = jsonObject['clients'];
-  const clients = allClients.filter(
-    (client) =>
-      client.membershiplevel === 'silver' || client.membershiplevel === 'gold'
+  const allCompanies = jsonObject['clients'];
+  console.log(allCompanies)
+  const companies = allCompanies.filter(
+    (company) =>
+      company.membership === 'Silver' || company.membership === 'Gold'
   );
-  const index = Math.floor(Math.random() * (3 - 1 + 1) + 1);
+  const index = Math.floor(Math.random() * (4 - 1 + 1) + 1);
   companies.splice(index - 1, 1);
-  companies.forEach((client, index) => {
-    displayClients(client, index);
+  for (let i = companies.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [companies[i], companies[j]] = [companies[j], companies[i]];
+  }
+  companies.forEach((company, index) => {
+    displayCompanies(company, index);
   });
 }
 
-function displayClients(client, index) {
+function displayCompanies(company, index) {
   let spotlight = document.createElement('div');
   spotlight.innerHTML = `
   <div class="spotlight${index + 1} spotlight">
-    <h2>${client.name}</h2>
+    <h2>${company.name}</h2>
     <picture>
-      <img width="100" height="100" src="${client.imageUrl}" alt="${
-    client.name
+      <source srcset="${company.imageUrl}">
+      <img width="100" height="100" src="${company.imageUrl}" alt="${
+    company.name
   }">
     </picture>
     <div class="info">
-      <p>${client.address}</p>
+      <p>${company.address}</p>
       <p>Phone: ${company.phonenumber}</p>
-      <a target="_blank" href="${client.website}">${client.website}</a>
+      <a target="_blank" href="${company.website}">${company.website}</a>
     </div>
   </div>
   `;
@@ -143,3 +148,47 @@ function displayClients(client, index) {
 }
 
 const prove = getData();
+
+console.log(getData) */
+
+const requestURL = "https://pangloss1107.github.io/chamber/directory/scripts/data.json";
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    console.table(jsonObject); // temporary checking for valid response and data parsing
+    const clients = jsonObject["clients"];
+    
+    for (let i = 0; i < 3; i++) {
+        let clientsbox = document.createElement("div");      
+        let p1 = document.createElement("h3");
+        let portrait = document.createElement("img");
+        let p3 = document.createElement("p");
+        let p4 = document.createElement("p");
+        let web = document.createElement("a")
+
+      clientsbox.setAttribute('class', 'clients');
+      p1.textContent = clients[i].name;
+      p1.setAttribute('class', 'par1')
+      portrait.setAttribute('src', clients[i].imageUrl);
+      portrait.setAttribute('alt', 'Image of ' + clients[i].name + ' company');
+      portrait.setAttribute('loading', 'lazy');
+      p3.textContent = clients[i].adress;
+      p3.setAttribute('class', 'adressline');
+      p4.textContent = `Phone number: ${clients[i].phonenumber}`;
+      p4.setAttribute('class', 'pnumber')
+      web.setAttribute('href', clients[i].website);
+      web.textContent = clients[i].website
+
+      clientsbox.appendChild(p1);
+      clientsbox.appendChild(portrait);
+      clientsbox.appendChild(p3);
+      clientsbox.appendChild(p4);
+      clientsbox.appendChild(web);
+
+
+      document.querySelector("div.spotlights").appendChild(clientsbox);
+    }
+  });
